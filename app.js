14641bb4de79f3a040a75f61af2aa46c9f110151
Syedayyan168 <syedayyanalimadni@gmail.com>
2025-04-23 00:30:44 -0400

    currency converter

---
+++ app.js
@@ -0,0 +1,4 @@
+
+  document.getElementById("startBtn").addEventListener("click", function () {
+    window.location.href = "currency converter/converter.html"; // replace with your target page
+  });
